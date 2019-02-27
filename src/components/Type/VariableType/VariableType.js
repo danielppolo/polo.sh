3261222fe3d3b2d1e05e9a42aa580b3546933007
Daniel Polo <od.polo@gmail.com>
2019-02-27 07:24:40 -0600

    Fx in listeners for variable font

--- src/components/Type/VariableType/VariableType.js
+++ src/components/Type/VariableType/VariableType.js
@@ -5,7 +5,7 @@ const variableType = props => {
 
     useEffect(() => {
         const title = document.querySelector('#variable');
-        document.addEventListener('mousemove touchmove', event => {
+        const resize = event => {
             const x = event.clientX;
             const y = event.clientY;
             const height = window.innerHeight;
@@ -13,7 +13,10 @@ const variableType = props => {
             const wght = ((x * 800) / width) + 100;  
             const wdth = ((y * 200) / height);  
             title.style.fontVariationSettings = `'wght' ${wght}, 'wdth' ${wdth}`;
-        });
+        };
+        
+        document.addEventListener('mousemove', resize);
+        document.addEventListener('touchmove', resize);
     });
 
     return <h1 id='variable'>{props.children}</h1>;
