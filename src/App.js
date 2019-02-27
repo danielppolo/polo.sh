6e32719dd3f0eb922c75c019f7ccdf4ccfbf7524
Daniel Polo <od.polo@gmail.com>
2019-02-27 07:14:18 -0600

    Agrandir added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 import Slides from './components/Slides/Slides';
 import Slide from './components/Slides/Slide/Slide';
 
 class App extends Component {
+
+  componentDidMount(){
+    const title = document.querySelector('#title');
+    document.addEventListener('mousemove', event => {
+      const x = event.clientX;
+      const y = event.clientY;
+      const height = window.innerHeight;
+      const width = window.innerWidth;
+      const wght = ((x * 800) / width) + 100;  
+      const wdth = ((y * 200) / height);  
+      title.style.fontVariationSettings = `'wght' ${wght}, 'wdth' ${wdth}`;
+    });
+  }
+
   render() {
     return (
-      <Slides>
-        <Slide>
-          <Slide>
-            <h1>Hello Mexico!</h1>
-          </Slide>
-          <Slide>
-            <h1>Hello CDMX!</h1>
-          </Slide>
-        </Slide>
-        <Slide>
-            <h1>Hello daniel!</h1>
-        </Slide>
-      </Slides>
+      <div className={style.App}>
+        <div className={style.AppHeader}>
+          <h1 id='title'>polo.sh</h1>
+        </div>
+      </div>
     );
   }
 }
--- src/components/Slides/Slides.js
+++ src/components/Slides/Slides.js
@@ -14,15 +14,15 @@ class Slides extends Component {
             transition: 'convex', // none/fade/slides/convex/concave/zoom
     
             // More info https://github.com/hakimel/reveal.js#dependencies
-            dependencies: [
-              { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-              { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-              { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-            //   { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-              { src: 'plugin/search/search.js', async: true },
-              { src: 'plugin/zoom-js/zoom.js', async: true },
-              { src: 'plugin/notes/notes.js', async: true }
-            ]
+            // dependencies: [
+            //   { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+            //   { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            //   { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            // //   { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+            //   { src: 'plugin/search/search.js', async: true },
+            //   { src: 'plugin/zoom-js/zoom.js', async: true },
+            //   { src: 'plugin/notes/notes.js', async: true }
+            // ]
           });
     }
     
