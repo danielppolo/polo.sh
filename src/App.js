a711635ff2e3841a4c7af09b8e395f41bc50eb2b
Daniel Polo <od.polo@gmail.com>
2019-02-28 23:48:04 -0600

    Introducing views with Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import style from './App.module.scss';
-import Slides from './components/Slides/Slides';
-import Slide from './components/Slides/Slide/Slide';
 import VariableType from './components/Type/VariableType/VariableType';
 import DynamicText from './components/Type/DynamicText/DynamicText';
 
@@ -11,10 +9,18 @@ class App extends Component {
   render() {
     return (
       <div className={style.App}>
-        <div className={style.AppHeader}>
+          <Switch>
+            <Route path='/talks' exact component={}/>
+            <Route path='/playground' exact component={}/>
+            <Route path='/history' exact component={}/>
+            <Route path='/ideas' exact component={}/>
+            <Route path='/projects' exact component={}/>
+            <Route path='/space' exact component={}/>
+            <Route path='/archive' exact component={}/>
+            <Route path='/404' exact component={}/>
+            <Route path='/' component={}/>
+          </Switch>
           <VariableType><DynamicText strings={['Hello world!', 'I\'m not a bot']}/></VariableType>
-          
-        </div>
       </div>
     );
   }
